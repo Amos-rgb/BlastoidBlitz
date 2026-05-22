9a10a901a199ade9ba6bf5943f3722610b419837
AmosKirkendoll <sample@bths.edu>
2026-05-22 10:59:27 -0400

    commit

--- src/Bomb.java
+++ src/Bomb.java
@@ -1,2 +1,19 @@
+import java.awt.*;
+
 public class Bomb {
+    public int x;
+    public int y;
+    private int countdown;
+    private Player player;
+    public Bomb(int x, int y, Player player) {
+        this.x = x;
+        this.y = y;
+        this.player = player;
+        countdown = 125;
+    }
+    public Rectangle explosion() {
+        if (countdown == 0) return new Rectangle(x-64,y-64,192,192);
+        countdown--;
+        return new Rectangle(-1,-1,0,0);
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,14 +2,20 @@ import java.awt.image.BufferedImage;
 
 public class Player {
     public int x;
-    public int y;
+    public int y;;
     private int maxHealth;
     private int health;
+    private int score;
     private BufferedImage sprite;
     public Player(int x, int y) {
         this.x = x;
         this.y = y;
         maxHealth = 1;
         health = maxHealth;
+        score = 0;
+    }
+
+    public void placeBomb() {
+
     }
 }
